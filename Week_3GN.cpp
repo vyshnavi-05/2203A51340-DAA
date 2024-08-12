91eec7b7bc671a53820c06333811399e81d007be
vyshnavi-05 <133511968+vyshnavi-05@users.noreply.github.com>
2024-08-12 23:19:28 +0530

    Add files via upload

---
+++ Week_3GN.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int compareAsc(const void *a, const void *b) 
+{
+    return (*(int*)a - *(int*)b);
+}
+int main() 
+{
+    int orders[100];
+    int n = 0,i;
+    char input[1000];
+    char *token;
+    printf("Enter orders Priority Number (comma-separated):\n");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+    token = strtok(input, ",");
+    while (token != NULL) 
+    {
+        orders[n++] = atoi(token);
+        token = strtok(NULL, ",");
+    }
+    qsort(orders, n, sizeof(int), compareAsc);
+    printf("After Sorting Orders Priority:\n");
+    for (i = 0; i < n; i++) 
+    {
+        if (i > 0) printf(",");
+        printf("%d", orders[i]);
+    }
+    printf("\n");
+    return 0;
+}
